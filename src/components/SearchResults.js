0036bc4231a8533ce8b091d4e5c8bfc3aabe76cf
harshit960 <raj.harshit960@gmail.com>
2022-09-16 19:44:33 +0530

    0.0.6

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -13,14 +13,13 @@ export default function SearchResults() {
   useEffect(() => async () => {
     if (loading) {
       
-      let url = "https://grabn-api.herokuapp.com/search/tv";
+      let url = "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query=lg";
       
       let data = await fetch(url, {
         headers: {
             'Fk-Affiliate-Id': 'harshit960',
             'Fk-Affiliate-Token': '403145a980b743eba1582a1626407ba4',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            
         }
     });
       let passedData = await data.json();
