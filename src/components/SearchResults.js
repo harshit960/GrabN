3882d8c5b3545ec9fa2f26302a7ac5c5717fe6c9
harshit960 <raj.harshit960@gmail.com>
2022-09-17 13:21:07 +0530

    try

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,80 +4,59 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function SearchResults() {
-  const [itemList, setitemList] = useState([1, 2, 3, 4]);
-  const [name, setname] = useState("....");
+  const [itemList, setitemList] = useState([]);
+  const [product, setproduct] = useState("....");
   const [loading, setloading] = useState(true);
   const [searchQuery, setsearchQuery] = useState("");
   const { id } = useParams();
 
   useEffect(() => async () => {
-    
-      let url =
-        "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query=lg";
-      const axios = require("axios");
-
-      const response = await axios.get(
-        "https://affiliate-api.flipkart.net/affiliate/1.0/search.json",
-        {
-          params: {
-            query: "spiderman",
-          },
-          headers: {
-            "Fk-Affiliate-Id": "harshit960",
-            "Fk-Affiliate-Token": "403145a980b743eba1582a1626407ba4",
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
-      );
-      console.log(response);
-      let data = fetch(url, {
-        headers: {
-          "Fk-Affiliate-Id": "harshit960",
-          "Fk-Affiliate-Token": "403145a980b743eba1582a1626407ba4",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      console.log(data);
+    if (loading) {
+      let url = "https://grabn-api.herokuapp.com/search/" + id;
+      let data = await fetch(url);
       let passedData = await data.json();
-      setitemList(passedData);
+      setitemList(passedData.products);
       setloading(false);
-    
+    }
   });
 
   return (
     <>
       <ShortNFilterBar />
       <div className=" flex flex-wrap  pt-16">
-        {itemList.map((item) => (
-          <div class="flex justify-center w-1/2 mx-0 border-l border-b p-0 h-72 ">
-            {() => setname(item.title)}
+        {itemList.map((item) => {
+          return (
+            <div class="flex justify-center w-1/2 mx-0 border-l border-b p-0 h-72 ">
+              <div class="shadow-sm bg-white max-w-sm">
+                <a href={item.link}>
+                  <img
+                    class="max-h-44"
+                    src={
+                      item.thumbimages
+                        ? item.thumbimages
+                        : "https://www.dpcanashik.com/uploads/default/blank.png"
+                    }
+                    alt=""
+                  />
 
-            <div class="shadow-sm bg-white max-w-sm">
-              <a href={item.link}>
-                <img
-                  class="max-h-44"
-                  src={
-                    item.thumbimages
-                      ? item.thumbimages
-                      : "https://www.dpcanashik.com/uploads/default/blank.png"
-                  }
-                  alt=""
-                />
+                  <div class="p-2">
+                    <h5 class="text-gray-700 text-sm font-medium mb-0 ">
+                      {item.productBaseInfoV1.title}
+                    </h5>
 
-                <div class="p-2">
-                  <h5 class="text-gray-700 text-sm font-medium mb-0 "></h5>
-                  <p class="text-gray-500 text-sm mb-0.5 indent-0"></p>
-                  <h5 class="text-gray-800 text-sm font-medium mb-0 ">
-                    {"₹" + item.current_price}
-                  </h5>
-                  <h5 class="text-gray-400 text-sm font-medium mb-0 line-through">
-                    {"₹" + item.original_price}
-                  </h5>
-                </div>
-              </a>
+                    <p class="text-gray-500 text-sm mb-0.5 indent-0"></p>
+                    <h5 class="text-gray-800 text-sm font-medium mb-0 ">
+                      {"₹" + item.current_price}
+                    </h5>
+                    <h5 class="text-gray-400 text-sm font-medium mb-0 line-through">
+                      {"₹" + item.original_price}
+                    </h5>
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
